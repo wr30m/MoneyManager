40a0d4d854760a6f89b10359a773f541b9bdc780
kirazavrik <wr30mg@gmail.com>
2017-12-29 23:00:41 +0300

    add balance_fragment design

---
+++ app/src/main/java/com/wr30mggmail/moneymanager/BalanceFragment.java
@@ -0,0 +1,29 @@
+package com.wr30mggmail.moneymanager;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class BalanceFragment extends Fragment {
+
+
+    public BalanceFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_balance, container, false);
+    }
+
+
+}
